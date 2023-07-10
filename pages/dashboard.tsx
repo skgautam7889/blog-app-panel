b79931ab0ccd06f66cbd18110203228f4e67fb44
surajkumargautam7889@gmail.com <skumarlko2023@gmail.com>
2023-07-10 08:14:37 +0530

    add static header and footer

---
+++ pages/dashboard.tsx
@@ -0,0 +1,14 @@
+import Footer from "../components/layouts/Footer";
+import Header from "../components/layouts/Header";
+
+export default function DashBoard() {
+    return (
+        <>
+            <Header />
+
+            <h1>mmm</h1>
+
+            <Footer />
+        </>
+    )
+}
